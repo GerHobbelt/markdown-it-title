e6c63a0395719dbeaa842dd8fa9143c68898d973
Ger Hobbelt <ger@hobbelt.com>
2020-05-10 15:45:53 +0200

    - fix bugs introduced by merge - augment the title extraction to include inline code chunks; see the first test case - refactored tests as mocha tests for better reporting and diagnosis - added tests to ensure inline HTML is used as well (in `innerText`-like form: you will need to set `options.html = true` for markdown-it for this, though)

--- index.js
+++ index.js
@@ -2,23 +2,24 @@
 'use strict';
 
 
-module.exports = function plugin(md, level = 1) {
-  const originalHeadingOpen = md.renderer.rules.heading_open
+module.exports = function plugin(md, options) {
+  const level = ('level' in (options || {})) ? options.level : 1;
+  const originalHeadingOpen = md.renderer.rules.heading_open;
 
   md.renderer.rules.heading_open = function (...args) {
-    const [ tokens, idx, , env, self ] = args
+    const [ tokens, idx, , env, self ] = args;
 
     if (!env.title && (level < 1 || tokens[idx].tag === `h${level}`)) {
       env.title = tokens[idx + 1].children
-        .filter(t => t.type === 'text')
-        .reduce((acc, t) => acc + t.content, '')
+        .filter(t => t.type === 'text' || t.type === 'code_inline')
+        // do NOT select 'html_inline' as that node type will only carry the HTML open & close tags, *without* the innerText!
+        .reduce((acc, t) => acc + t.content, '');
     }
 
     // Execute original rule.
     if (originalHeadingOpen) {
-      return originalHeadingOpen.apply(this, args)
-    } else {
-      return self.renderToken(...args)
+      return originalHeadingOpen.apply(this, args);
     }
-  }
-}
+    return self.renderToken(...args);
+  };
+};
--- test/test.js
+++ test/test.js
@@ -1,32 +1,62 @@
-const { strictEqual: equal } = require('assert')
-const md = require('markdown-it')
-const title = require('./')
+'use strict';
+
+/*eslint-env mocha*/
+
+const { strictEqual: equal } = require('assert');
+const md = require('@gerhobbelt/markdown-it');
+const title = require('../');
 
 const render = (engine, src) => {
-  const env = {}
-  engine.render(src, env)
-  return env
-}
+  const env = {};
+  engine.render(src, env);
+  return env;
+};
 
-const engine = md({ typographer: true })
-  .use(title)
+const engine = md({ typographer: true, html: true })
+  .use(title);
 
-equal(
-  render(engine, '## H2\n\n# Hello, *`world`!(c)*').title,
-  'Hello, world!©'
-)
+describe('markdown-it-title', function () {
+  it('extracts entire H1 title, including inline code formatted parts', function () {
+    equal(
+      render(engine, '## H2\n\n# Hello, *`world`!(c)*').title,
+      'Hello, world!©'
+    );
+  });
 
-equal(
-  render(engine, '## H2\n\n# Instance reuse').title,
-  'Instance reuse'
-)
+  it("finds first H1, even when it's not the first header", function () {
+    equal(
+      render(engine, '## H2\n\n# Instance reuse').title,
+      'Instance reuse'
+    );
+  });
 
-equal(
-  render(md().use(title, 0), '## H2\n\n# H1').title,
-  'H2'
-)
+  it('picks first header when options.level = 0', function () {
+    equal(
+      render(md().use(title, { level:0 }), '## H2\n\n# H1').title,
+      'H2'
+    );
+  });
 
-equal(
-  render(md().use(title, 2), '# H1\n\n## H2').title,
-  'H2'
-)
+  it('picks first H2 header when options.level = 2', function () {
+    equal(
+      render(md().use(title, { level:2 }), '# H1\n\n## H2').title,
+      'H2'
+    );
+  });
+
+  it('extracts entire title, including inline HTML parts', function () {
+    equal(
+      render(engine, '## H2\n\n# Hello, <span><span> /woild/ </span></span> *!!*').title,
+      'Hello,  /woild/  !!'
+    );
+  });
+
+  it('extracts inline HTML as-is, when options.html is not set for markdown-it', function () {
+    equal(
+      render(md().use(title), '## H2\n\n# Hello, <span><span> /woild/ </span></span> *!!*').title,
+      'Hello, <span><span> /woild/ </span></span> !!'
+    );
+  });
+
+});
+
